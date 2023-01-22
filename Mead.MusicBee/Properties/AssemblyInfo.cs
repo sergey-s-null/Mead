9125a36a2113496beb01a498660ad9d7ae7be06e
Sergey Semenov <Laiser399@mail.ru>
2023-01-22 09:54:25 +0300

    Set description and company

--- Mead.MusicBee/Properties/AssemblyInfo.cs
+++ Mead.MusicBee/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mead.MusicBee")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library with MusicBee Api for plugins development.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sergey Semenov")]
 [assembly: AssemblyProduct("Mead.MusicBee")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
