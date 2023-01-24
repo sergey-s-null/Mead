aceae29722ee31386f48925777c07cb54b33df96
Sergey Semenov <Laiser399@mail.ru>
2023-01-24 19:38:10 +0300

    Split system enums

--- Mead.MusicBee/Enums/PluginType.cs
+++ Mead.MusicBee/Enums/PluginType.cs
@@ -1,18 +1,6 @@
-﻿// ReSharper disable InconsistentNaming
+// ReSharper disable InconsistentNaming
 
 namespace Mead.MusicBee.Enums;
-
-public enum MusicBeeVersion
-{
-    v2_0 = 0,
-    v2_1 = 1,
-    v2_2 = 2,
-    v2_3 = 3,
-    v2_4 = 4,
-    v2_5 = 5,
-    v3_0 = 6,
-    v3_1 = 7
-}
 
 public enum PluginType
 {
