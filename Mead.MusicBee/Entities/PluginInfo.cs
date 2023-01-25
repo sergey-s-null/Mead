228312f1a9a2908caed40e5c7eeff8beaacceff6
Sergey Semenov <Laiser399@mail.ru>
2023-01-25 21:20:58 +0300

    Add summary in PluginInfo

--- Mead.MusicBee/Entities/PluginInfo.cs
+++ Mead.MusicBee/Entities/PluginInfo.cs
@@ -11,12 +11,22 @@ public sealed class PluginInfo
     public string Name = "";
     public string Description = "";
     public string Author = "";
+
+    /// <summary>
+    /// The name of a Plugin Storage device or panel header for a dockable panel.
+    /// </summary>
     public string TargetApplication = "";
+
     public short VersionMajor;
     public short VersionMinor;
     public short Revision;
     public short MinInterfaceVersion;
     public short MinApiRevision;
     public ReceiveNotificationFlags ReceiveNotifications;
+
+    /// <summary>
+    /// Height in pixels that MusicBee should reserve in a panel for config settings.
+    /// When set, a handle to an empty panel will be passed to the Configure function of a Plugin class.
+    /// </summary>
     public int ConfigurationPanelHeight;
 }
