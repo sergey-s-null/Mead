102b00e21a1bde4249b973f7e743dbc843f871d1
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 10:56:02 +0300

    Add TypeExtensions

---
+++ Mead.MusicBee.MetaInfo/Extensions/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mead.MusicBee.MetaInfo.Extensions;
+
+internal static class TypeExtensions
+{
+    public static Type RemoveRefWrapper(this Type type)
+    {
+        if (type.IsByRef && type.HasElementType)
+        {
+            return type.GetElementType()!;
+        }
+
+        return type;
+    }
+}
