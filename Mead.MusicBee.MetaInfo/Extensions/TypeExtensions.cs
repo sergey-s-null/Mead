0beab65745f1c8d966c2ddde0e6e8ae7994f7932
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 11:08:58 +0300

    Add method and parameter definition extensions

--- Mead.MusicBee.MetaInfo/Extensions/TypeExtensions.cs
+++ Mead.MusicBee.MetaInfo/Extensions/TypeExtensions.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mead.MusicBee.MetaInfo.Extensions;
 
 internal static class TypeExtensions
 {
-    public static Type RemoveRefWrapper(this Type type)
+    private static readonly IReadOnlyDictionary<Type, string> BaseTypesMappings = new Dictionary<Type, string>
+    {
+        [typeof(string)] = "string",
+        [typeof(bool)] = "bool",
+        [typeof(void)] = "void",
+        [typeof(int)] = "int",
+        [typeof(byte)] = "byte",
+        [typeof(float)] = "float",
+        [typeof(double)] = "double",
+        [typeof(object)] = "object"
+    };
+
+    /// <summary>
+    /// Return C# type alias instead of .Net CLR type:
+    ///     String -> string
+    ///     Int32 -> int
+    /// </summary>
+    public static string GetCSharpAlias(this Type type)
+    {
+        return BaseTypesMappings.TryGetValue(type, out var stringType)
+            ? stringType
+            : type.Name;
+    }
+
+    internal static Type RemoveRefWrapper(this Type type)
     {
         if (type.IsByRef && type.HasElementType)
         {
