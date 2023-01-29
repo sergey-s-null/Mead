28ea34180ce29592b88f0d18d270493b093ba224
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 10:56:09 +0300

    Add EnumerableExtensions

---
+++ Mead.MusicBee.MetaInfo/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mead.MusicBee.MetaInfo.Extensions;
+
+internal static class EnumerableExtensions
+{
+    public static IEnumerable<TDest> CastOrSkip<TSource, TDest>(this IEnumerable<TSource> items)
+    {
+        return items
+            .Where(x => x is TDest)
+            .Cast<TDest>();
+    }
+}
