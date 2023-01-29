c57d6406df228ffe89689942ebad313b83778980
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 10:56:32 +0300

    Add FieldInfoExtensions

---
+++ Mead.MusicBee.MetaInfo/Extensions/FieldInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace Mead.MusicBee.MetaInfo.Extensions;
+
+internal static class FieldInfoExtensions
+{
+    public static bool IsDelegate(this FieldInfo fieldInfo)
+    {
+        return typeof(Delegate).IsAssignableFrom(fieldInfo.FieldType);
+    }
+}
