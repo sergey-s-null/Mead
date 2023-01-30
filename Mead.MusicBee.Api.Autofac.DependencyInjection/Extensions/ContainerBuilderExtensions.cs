f3edeea8bbd4cc2b946c54a6d52c0d2e8e8028fe
Sergey Semenov <Laiser399@mail.ru>
2023-01-30 22:36:29 +0300

    Add registration extension methods

---
+++ Mead.MusicBee.Api.Autofac.DependencyInjection/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Mead.MusicBee.Api.Services;
+using Mead.MusicBee.Api.Services.Abstract;
+using Mead.MusicBee.Services;
+
+namespace Mead.MusicBee.Api.Autofac.DependencyInjection.Extensions;
+
+public static class ContainerBuilderExtensions
+{
+    public static void RegisterMusicBeeApi(
+        this ContainerBuilder builder,
+        MusicBeeApiMemoryContainer musicBeeApiMemoryContainer)
+    {
+        builder
+            .RegisterType<MusicBeeApiMemoryContainerWrapper>()
+            .WithParameter(TypedParameter.From(musicBeeApiMemoryContainer))
+            .As<IMusicBeeApi>()
+            .SingleInstance();
+    }
+
+    public static void RegisterMusicBeeApi(this ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<MusicBeeApiMemoryContainerWrapper>()
+            .As<IMusicBeeApi>()
+            .SingleInstance();
+    }
+}
