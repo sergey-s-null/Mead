29236648c19d5318394a4ceeefba3f2254484e7b
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 00:46:18 +0300

    Add IFieldValuesConverter

---
+++ Mead.MusicBee.LibraryApi/Services/Abstract/IFieldValuesConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Mead.MusicBee.LibraryApi.Services.Abstract;
+
+public interface IFieldValuesConverter
+{
+    string ConvertBoolToString(bool value);
+    bool ConvertStringToBool(string value);
+
+    string ConvertNumberToString(long value);
+    long ConvertStringToNumber(string value);
+
+    string ConvertDateToString(DateTime value);
+    DateTime ConvertStringToDate(string value);
+
+    string ConvertEnumToString(IReadOnlyList<string> value);
+    IReadOnlyList<string> ConvertStringToEnum(string value);
+
+    string ConvertRatingToString(Rating value);
+    Rating ConvertStringToRating(string value);
+}
