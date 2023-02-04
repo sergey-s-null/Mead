0eef1e4a0349159e7996595ea2cb38f77c12d6f5
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 00:43:30 +0300

    Add VariantField, ReadOnlyVariantField

---
+++ Mead.MusicBee.LibraryApi/Entities/ReadOnlyVariantField.cs
@@ -0,0 +1,36 @@
+﻿using Mead.MusicBee.LibraryApi.Entities.Abstract;
+using Mead.MusicBee.LibraryApi.Services.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Entities;
+
+public sealed class ReadOnlyVariantField : IReadOnlyVariantField
+{
+    public string StringValue => _getTagValue();
+
+    public bool BoolValue =>
+        _fieldValuesConverter.ConvertStringToBool(_getTagValue());
+
+    public long NumberValue =>
+        _fieldValuesConverter.ConvertStringToNumber(_getTagValue());
+
+    public DateTime DateValue =>
+        _fieldValuesConverter.ConvertStringToDate(_getTagValue());
+
+    public IReadOnlyList<string> EnumValue =>
+        _fieldValuesConverter.ConvertStringToEnum(_getTagValue());
+
+    public Rating RatingValue =>
+        _fieldValuesConverter.ConvertStringToRating(_getTagValue());
+
+    private readonly Func<string> _getTagValue;
+
+    private readonly IFieldValuesConverter _fieldValuesConverter;
+
+    public ReadOnlyVariantField(
+        Func<string> getTagValue,
+        IFieldValuesConverter fieldValuesConverter)
+    {
+        _getTagValue = getTagValue;
+        _fieldValuesConverter = fieldValuesConverter;
+    }
+}
---
+++ Mead.MusicBee.LibraryApi/Entities/VariantField.cs
@@ -0,0 +1,57 @@
+﻿using Mead.MusicBee.LibraryApi.Entities.Abstract;
+using Mead.MusicBee.LibraryApi.Services.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Entities;
+
+public sealed class VariantField : IVariantField
+{
+    public string StringValue
+    {
+        get => _getTagValue();
+        set => _setTagValue(value);
+    }
+
+    public bool BoolValue
+    {
+        get => _fieldValuesConverter.ConvertStringToBool(_getTagValue());
+        set => _setTagValue(_fieldValuesConverter.ConvertBoolToString(value));
+    }
+
+    public long NumberValue
+    {
+        get => _fieldValuesConverter.ConvertStringToNumber(_getTagValue());
+        set => _setTagValue(_fieldValuesConverter.ConvertNumberToString(value));
+    }
+
+    public DateTime DateValue
+    {
+        get => _fieldValuesConverter.ConvertStringToDate(_getTagValue());
+        set => _setTagValue(_fieldValuesConverter.ConvertDateToString(value));
+    }
+
+    public IReadOnlyList<string> EnumValue
+    {
+        get => _fieldValuesConverter.ConvertStringToEnum(_getTagValue());
+        set => _setTagValue(_fieldValuesConverter.ConvertEnumToString(value));
+    }
+
+    public Rating RatingValue
+    {
+        get => _fieldValuesConverter.ConvertStringToRating(_getTagValue());
+        set => _setTagValue(_fieldValuesConverter.ConvertRatingToString(value));
+    }
+
+    private readonly Func<string> _getTagValue;
+    private readonly Action<string> _setTagValue;
+    private readonly IFieldValuesConverter _fieldValuesConverter;
+
+    public VariantField(
+        Func<string> getTagValue,
+        Action<string> setTagValue,
+        IFieldValuesConverter fieldValuesConverter)
+    {
+        _getTagValue = getTagValue;
+        _setTagValue = setTagValue;
+        _fieldValuesConverter = fieldValuesConverter;
+    }
+}
