cc841e78c39bc2ef3267746eef659f2dee478f65
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 00:38:55 +0300

    Add IVariantField, IReadOnlyVariantField

---
+++ Mead.MusicBee.LibraryApi/Entities/Abstract/IReadOnlyVariantField.cs
@@ -0,0 +1,12 @@
+﻿namespace Mead.MusicBee.LibraryApi.Entities.Abstract;
+
+public interface IReadOnlyVariantField
+{
+    string StringValue { get; }
+
+    bool BoolValue { get; }
+    long NumberValue { get; }
+    DateTime DateValue { get; }
+    IReadOnlyList<string> EnumValue { get; }
+    Rating RatingValue { get; }
+}
---
+++ Mead.MusicBee.LibraryApi/Entities/Abstract/IVariantField.cs
@@ -0,0 +1,12 @@
+﻿namespace Mead.MusicBee.LibraryApi.Entities.Abstract;
+
+public interface IVariantField
+{
+    string StringValue { get; set; }
+
+    bool BoolValue { get; set; }
+    long NumberValue { get; set; }
+    DateTime DateValue { get; set; }
+    IReadOnlyList<string> EnumValue { get; set; }
+    Rating RatingValue { get; set; }
+}
