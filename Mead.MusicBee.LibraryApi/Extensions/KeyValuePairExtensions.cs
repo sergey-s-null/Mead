69c495456beb15555ead12a85739ba7c507e04d6
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 00:45:56 +0300

    Add KeyValuePair deconstruction method

---
+++ Mead.MusicBee.LibraryApi/Extensions/KeyValuePairExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Mead.MusicBee.LibraryApi.Extensions;
+
+internal static class KeyValuePairExtensions
+{
+    public static void Deconstruct<TKey, TValue>(
+        this KeyValuePair<TKey, TValue> keyValuePair,
+        out TKey key,
+        out TValue value)
+    {
+        key = keyValuePair.Key;
+        value = keyValuePair.Value;
+    }
+}
