2aa22d3fc1615f2f614c9f88cff9ac8aa16ecb15
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 00:45:08 +0300

    Add factories

---
+++ Mead.MusicBee.LibraryApi/Factories/Abstract/ReadOnlyVariantFieldFactory.cs
@@ -0,0 +1,5 @@
+﻿using Mead.MusicBee.LibraryApi.Entities.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Factories.Abstract;
+
+public delegate IReadOnlyVariantField ReadOnlyVariantFieldFactory(Func<string> getTagValue);
---
+++ Mead.MusicBee.LibraryApi/Factories/Abstract/VariantFieldFactory.cs
@@ -0,0 +1,8 @@
+﻿using Mead.MusicBee.LibraryApi.Entities.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Factories.Abstract;
+
+public delegate IVariantField VariantFieldFactory(
+    Func<string> getTagValue,
+    Action<string> setTagValue
+);
