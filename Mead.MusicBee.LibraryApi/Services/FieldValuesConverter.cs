19aefb8510959bd315a07f2269de92dc0d17f3be
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 10:15:07 +0300

    Add not implemented FieldValuesConverter

---
+++ Mead.MusicBee.LibraryApi/Services/FieldValuesConverter.cs
@@ -0,0 +1,57 @@
+﻿using Mead.MusicBee.LibraryApi.Enums;
+using Mead.MusicBee.LibraryApi.Services.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Services;
+
+public class FieldValuesConverter : IFieldValuesConverter
+{
+    public string ConvertBoolToString(bool value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ConvertStringToBool(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ConvertNumberToString(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long ConvertStringToNumber(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ConvertDateToString(DateTime value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime ConvertStringToDate(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ConvertEnumToString(IReadOnlyList<string> value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyList<string> ConvertStringToEnum(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ConvertRatingToString(Rating value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Rating ConvertStringToRating(string value)
+    {
+        throw new NotImplementedException();
+    }
+}
