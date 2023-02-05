48a3520f4131368001649e5ec689e50ea11f9257
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 10:21:29 +0300

    Add registrations for library api

---
+++ Mead.MusicBee.LibraryApi.Autofac.DependencyInjection/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Mead.MusicBee.LibraryApi.Entities;
+using Mead.MusicBee.LibraryApi.Entities.Abstract;
+using Mead.MusicBee.LibraryApi.Services;
+using Mead.MusicBee.LibraryApi.Services.Abstract;
+
+namespace Mead.MusicBee.LibraryApi.Autofac.DependencyInjection.Extensions;
+
+public static class ContainerBuilderExtensions
+{
+    public static void RegisterMusicBeeLibraryApi(this ContainerBuilder containerBuilder)
+    {
+        containerBuilder
+            .RegisterType<MusicBeeLibraryApi>()
+            .As<IMusicBeeLibraryApi>()
+            .SingleInstance();
+
+        containerBuilder
+            .RegisterType<CachingReadOnlyMusicFile>()
+            .As<IReadOnlyMusicFile>();
+        containerBuilder
+            .RegisterType<MusicFile>()
+            .As<IMusicFile>();
+        containerBuilder
+            .RegisterType<ReadOnlyVariantField>()
+            .As<IReadOnlyVariantField>();
+        containerBuilder
+            .RegisterType<VariantField>()
+            .As<IVariantField>();
+
+        containerBuilder
+            .RegisterType<FieldValuesConverter>()
+            .As<IFieldValuesConverter>()
+            .SingleInstance();
+    }
+}
