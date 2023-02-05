f87a3b92de49ef02955f4d5959647d267f93e4eb
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 12:19:58 +0300

    Fix DI

--- Mead.MusicBee.LibraryApi.Autofac.DependencyInjection/Extensions/ContainerBuilderExtensions.cs
+++ Mead.MusicBee.LibraryApi.Autofac.DependencyInjection/Extensions/ContainerBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Autofac.Core;
 using Mead.MusicBee.LibraryApi.Autofac.DependencyInjection.Enums;
 using Mead.MusicBee.LibraryApi.Entities;
 using Mead.MusicBee.LibraryApi.Entities.Abstract;
@@ -17,21 +18,38 @@ public static class ContainerBuilderExtensions
             .As<IMusicBeeLibraryApi>()
             .SingleInstance();
 
+        var normalEnumFieldConverterParameter = new ResolvedParameter(
+            (parameterInfo, _) => parameterInfo.Name == "enumFieldValueConverter",
+            (_, context) => context
+                .ResolveKeyed<IFieldValueConverter<IReadOnlyList<string>>>(EnumType.Normal)
+        );
+        var encodedEnumFieldConverterParameter = new ResolvedParameter(
+            (parameterInfo, _) => parameterInfo.Name == "encodedEnumFieldValueConverter",
+            (_, context) => context
+                .ResolveKeyed<IFieldValueConverter<IReadOnlyList<string>>>(EnumType.Encoded)
+        );
+        var enumFieldConverterParameters = new[] { normalEnumFieldConverterParameter, encodedEnumFieldConverterParameter };
+
         containerBuilder
             .RegisterType<CachingReadOnlyMusicFile>()
+            .WithParameters(enumFieldConverterParameters)
             .As<IReadOnlyMusicFile>();
         containerBuilder
             .RegisterType<MusicFile>()
+            .WithParameters(enumFieldConverterParameters)
             .As<IMusicFile>();
         containerBuilder
             .RegisterType<ReadOnlyVariantField>()
+            .WithParameters(enumFieldConverterParameters)
             .As<IReadOnlyVariantField>();
         containerBuilder
             .RegisterType<VariantField>()
+            .WithParameters(enumFieldConverterParameters)
             .As<IVariantField>();
 
         containerBuilder
             .RegisterType<BoolFieldValueConverter>()
+            .WithParameters(enumFieldConverterParameters)
             .As<IFieldValueConverter<bool>>()
             .SingleInstance();
         containerBuilder
