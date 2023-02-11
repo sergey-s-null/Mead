c172a4ff71f8c377c68d9aeb58bc84ddf24a36b9
Sergey Semenov <Laiser399@mail.ru>
2023-02-11 23:46:01 +0300

    Add MusicBeeApiServiceImpl, Plugin; Add package references.

---
+++ Mead.MusicBee.Remoting.Plugin/PluginContainer.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace Mead.MusicBee.Remoting.Plugin;
+
+public static class PluginContainer
+{
+    public static IContainer Create()
+    {
+        throw new NotImplementedException();
+    }
+}
